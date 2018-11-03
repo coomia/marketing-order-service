8fcf38babc07c431ee6ec9650d43e4de77b92636
jonyh <403774944@qq.com>
2018-11-04 03:31:19 +0800

    Adjust some logic.

--- src/main/java/com/meiye/util/ObjectUtil.java
+++ src/main/java/com/meiye/util/ObjectUtil.java
@@ -111,4 +111,13 @@ public class ObjectUtil {
         }
         return map;
     }
+
+    public static boolean equals(Object object1, Object object2) {
+        return object1 == object2?true:(object1 != null && object2 != null?object1.equals(object2):false);
+    }
+
+    public static boolean notEqual(Object object1, Object object2) {
+        return !equals(object1, object2);
+    }
+
 }
