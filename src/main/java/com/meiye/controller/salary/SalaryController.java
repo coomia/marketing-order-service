7740bcf9e5264ffd2a9e9e24b829f438158912e4
shenhuan <ad769622293@qq.com>
2018-11-04 16:59:38 +0800

    提成计算

--- src/main/java/com/meiye/controller/salary/SalaryController.java
+++ src/main/java/com/meiye/controller/salary/SalaryController.java
@@ -2,6 +2,7 @@ package com.meiye.controller.salary;
 
 import com.meiye.bo.salary.SalaryBo;
 import com.meiye.bo.system.ResetApiResult;
+import com.meiye.exception.BusinessException;
 import com.meiye.service.Salary.SalaryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +16,7 @@ import java.util.List;
  * @Version 1.0
  */
 @RestController
-@RequestMapping(value = "/public/api/salary",produces="application/json;charset=UTF-8")
+@RequestMapping(value = "/pos/api/salary",produces="application/json;charset=UTF-8")
 public class SalaryController {
 
     @Autowired
@@ -23,7 +24,11 @@ public class SalaryController {
 
     @PostMapping("/getAllSalary")
     public ResetApiResult getAllSalary(@RequestBody SalaryBo salaryBo){
-        List<SalaryBo> allSalary = salaryService.getAllSalary(salaryBo);
-        return ResetApiResult.sucess("");
+        try {
+            List<SalaryBo> allSalary = salaryService.getAllSalary(salaryBo);
+            return ResetApiResult.sucess(allSalary);
+        }catch (Exception e){
+            throw new BusinessException("获取绩效失败!");
+        }
     }
 }
